17eb9bf763877eea3f4f1e0f1a161719b0e6e55d
Xafron <admin@xafron.nl>
2025-10-22 00:00:00 +0000

    refactor: remove unnecessary comment in touch device detection

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 export const TOUCH_EXPAND_SUPPRESS_MS = 350;
 export const CLICK_EXPAND_SUPPRESS_MS = 250;
-// Dynamic touch detection to support testing
 export const isTouchDevice = () => {
   return (window.matchMedia && window.matchMedia('(hover: none) and (pointer: coarse)').matches) ||
     ('ontouchstart' in window) ||
